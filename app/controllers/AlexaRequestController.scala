17da24d98f87dbdf03d8e2760abc43b8fea17911
Brendan Machado <brendan.machado@octanner.com>
2017-10-16 14:06:58 -0600

    Corrected error and added handler for LaunchRequest

--- app/controllers/AlexaRequestController.scala
+++ app/controllers/AlexaRequestController.scala
@@ -32,7 +32,7 @@ class AlexaRequestController @Inject() (service: AlexaIntentService) extends Con
 
   private def createAlexaIntentAction(userId: String, customerId: String)(request: AlexaIntentRequest): Future[Result] = {
     for {
-      answer <- service.handleIntent(userId, customerId, request.request.intent.name)
+      answer <- service.handleIntent(userId, customerId, "")
     } yield {
       logger.info("response = " + answer)
       Ok
@@ -42,6 +42,23 @@ class AlexaRequestController @Inject() (service: AlexaIntentService) extends Con
   def brenDialogHandler = Action.async(parse.json) { request =>
     logger.info(request.body.toString())
     val alexaRequest = request.body.validate[AlexaIntentRequest].get
+    alexaRequest.request.`type` match {
+      case "LaunchRequest" =>
+        logger.info("launchRequest")
+        getGiveAnECardResponse()
+      case "IntentRequest" =>
+        logger.info("IntentRequest")
+      case "SessionEndedRequest" =>
+        logger.info("SessionEndedRequest")
+    }
     Future(Ok(Json.parse("""{"version":"1.0","sessionAttributes":{"supportedHoriscopePeriods":{"daily":true,"weekly":false,"monthly":false}},"response":{"outputSpeech":{"type":"PlainText","text":"Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless. Can I help you with anything else?"},"card":{"type":"Simple","title":"Horoscope","content":"Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."},"reprompt":{"outputSpeech":{"type":"PlainText","text":"Can I help you with anything else?"}},"shouldEndSession":true}}""")))
+  }
+
+  private def getGiveAnECardResponse(): Future[Result] = {
+    val firstNamePrompt = "Say the first name"
+    val speechOutput = "<speak>" + firstNamePrompt + "</speak>"
+    //val repromptText = "I can lead you through providing a city and " + "day of the week to get tide information, " + "or you can simply open Tide Pooler and ask a question like, " + "get tide information for Seattle on Saturday. " + "For a list of supported cities, ask what cities are supported. " + whichCityPrompt
+    //newAskResponse(speechOutput, true, repromptText, false)
+    Future(Ok(Json.parse("""{"version":"1.0","sessionAttributes":{},"response":{"outputSpeech":{"type":"PlainText","text":"Say the first name"},"card":{},"reprompt":{},"shouldEndSession":false}}""")))
   }
 }
